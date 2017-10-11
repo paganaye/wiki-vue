0d99c4ea653617cb62a21a0f08dc4fc54b21bf39
Pascal GANAYE <pascal2007@ganaye.com>
2017-10-12 00:17:27 +0200

    add vuetify

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import * as Vue from 'vue';
+import Vuetify from 'vuetify';
+
+Vue.use(Vuetify);
+
 
 Vue.component("cmpOne", {
     props: ["userName"],
-    template: '<p>hi {{userName}}</p>'
+    template: `<div>
+    <h3>hi {{userName}}</h3>
+    <div class="text-xs-center">
+    <v-badge right>
+      <span slot="badge">1</span>
+      <span>Examples here</span>
+    </v-badge>      
+  </div></div>`
 });
 
 
