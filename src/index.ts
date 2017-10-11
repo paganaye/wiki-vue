c8e49dabf186b2f5af5404598fc9554587c9f1b0
Pascal GANAYE <pascal2007@ganaye.com>
2017-10-11 23:48:44 +0200

    live components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import * as Vue from 'vue';
 
+Vue.component("cmpOne", {
+    template:'<p>hi</p>'
+
+});
+
 new Vue({
-    el:"#app",
-    template:"<div id='app'><h1>it works</h1></div>"
+    el: "#app",
+    template: `<div id='app'>
+    <h1>it works</h1>
+    <cmpOne><cmpOne>
+</div>`
 });
