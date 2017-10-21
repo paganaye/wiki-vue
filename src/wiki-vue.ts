862083937cbf2c9f2c242b8984d858a930549203
Pascal GANAYE <pascal2007@ganaye.com>
2017-10-21 10:28:56 +0200

    schema from db

--- src/wiki-vue.ts
+++ src/wiki-vue.ts
@@ -5,33 +5,33 @@ import Sortable = require('sortablejs');
 
 
 Vue.component("value-vue", {
-    props: ["label", "value", "type"],
+    props: ["label", "value", "schema"],
     template: `<div>
     <v-text-field
-        :label="label || (type && (type.label || type.name)) || '???'"
+        :label="label || (schema && (schema.label || schema.name)) || '???'"
         v-model="value"></v-text-field>
 </div>`
 });
 
 Vue.component("object-vue", {
-    props: ["label", "value", "type"],
+    props: ["label", "value", "schema"],
     template: `<div>
     <p>{{label}}</p>
-    <div v-for="prop in type.properties">
-        <dyn-vue :type='prop.type' :label='prop.label || prop.name' :value='value[prop.name]' />
+    <div v-for="prop in schema.properties">
+        <dyn-vue :schema='prop.schema' :label='prop.label || prop.name' :value='value[prop.name]' />
     </div>
 </div>`
 });
 
 Vue.component("array-vue", {
-    props: ["label", "value", "type"],
+    props: ["label", "value", "schema"],
     template: `<div>
     <p>{{label}}</p>
     <div  id="list" ref="list">
         <div v-for="(item, index) in value" class="array-item">
             <div class="array-item-handle">{{index}}</div>
             <div class="array-item-content">
-                <dyn-vue :label="'#' + index" :type='type && type.innerType || {}' :value='item' />
+                <dyn-vue :label="'#' + index" :schema='schema && schema.itemSchema || {}' :value='item' />
             </div>
             <v-btn color="secondary">Delete</v-btn>
         </div>    
@@ -50,20 +50,20 @@ Vue.component("array-vue", {
 });
 
 Vue.component("dyn-vue", {
-    props: ["label", "value", "type"],
+    props: ["label", "value", "schema"],
     template: `<div>
     <div v-if="debug">
-        <p>type:{{type}}</p>
+        <p>schema:{{schema}}</p>
         <p>label:{{label}}</p>
         <p>value:{{value}}</p>
     </div>
-    <component :is="componentType(value)" :label="label" :type="type" :value="value"></component>
+    <component :is="componentSchema(value)" :label="label" :schema="schema" :value="value"></component>
 </div>`,
     data: () => {
         return { debug: false };
     },
     computed: {
-        componentType: () => {
+        componentSchema: () => {
             // I should not need a value here but this.value is messed up by typescript
             return (value: any) => {
                 var result;
@@ -77,33 +77,24 @@ Vue.component("dyn-vue", {
     }
 });
 
-
 Vue.component("wiki-vue", {
-    props: ["type", "document"],
+    props: ["document"],
     template: `<div>
     <p>Loading table:'{{table}}'</p>
     <p>Loading id: '{{id}}'</p>
     <p>document: '{{document}}'</p>
     <p>value:'{{value}}'</p>
     <p>value-vue</p>
-    <dyn-vue label='xxx' :type='it3type' :value="value" />
-  
+    <dyn-vue label='xxx' :schema='schema' :value="value" />  
 </div>`,
     data: () => {
         return {
             value: "loading...",
-            it3type: {
-                "kind": "object",
-                "properties": [
-                    { "name": "A", "label": "first A" },
-                    { "name": "B", "label": "then B" },
-                    { "name": "C" }
-                ]
-            }
+            schema: null
         };
     },
     methods: {
-        loadFromDb: function (this: any) {
+        loadValueFromDb: function (this: any) {
             var that = this;
             var db = firebase.database();
             var ref = db.ref().child(this.table || "home");
@@ -118,6 +109,21 @@ Vue.component("wiki-vue", {
 
             }, function (errorObject: any) {
                 console.log("The read failed: " + errorObject.code);
+            });
+        },
+        loadSchemaFromDb: function (this: any) {
+            var that = this;
+            var db = firebase.database();
+            var ref = db.ref().child("schema");
+            if (this.id) ref = ref.child(this.table);
+
+            console.log("db", db, "ref", ref);
+            // Attach an asynchronous callback to read the data at our posts reference
+            ref.on("value", function (snapshot) {
+                that.schema = snapshot.val();
+                console.log("snapshot", that.schema);
+            }, function (errorObject: any) {
+                console.log("The schema read failed: " + errorObject.code);
             });
         }
     },
@@ -144,7 +150,8 @@ Vue.component("wiki-vue", {
     created: function (this: any) {
         console.log("created", "this", this, "table", this.table, "id", this.id);
         // Import Admin SDK
-        this.loadFromDb()
+        this.loadSchemaFromDb();
+        this.loadValueFromDb();
         // Get a database reference to our posts
     }
 });
